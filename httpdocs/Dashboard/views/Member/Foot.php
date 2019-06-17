9146df5ac61b132908b6110c4b0d9142061186d4
Jeyaraj1982 <38805527+Jeyaraj1982@users.noreply.github.com>
2019-06-17 09:26:12 +0530

    Removed unwanted files in Member Area

--- httpdocs/Dashboard/views/Member/Foot.php
+++
@@ -1,9 +0,0 @@
-<script src="<?php echo SiteUrl?>vendors/js/vendor.bundle.base.js"></script>
-  <script src="../../vendors/js/vendor.bundle.addons.js"></script>
-  <script src="<?php echo SiteUrl?>js/off-canvas.js"></script>
-  <script src="<?php echo SiteUrl?>js/hoverable-collapse.html"></script>
-  <script src="<?php echo SiteUrl?>js/misc.js"></script>
-  <script src="<?php echo SiteUrl?>js/settings.html"></script>
-  <script src="<?php echo SiteUrl?>js/todolist.html"></script>
-  </body>
-</html>
--- httpdocs/Dashboard/webservice.php
+++
@@ -1,8 +0,0 @@
-<?php
-     include_once("config.php");
-     $obj = new $_GET['m']();
-     echo $obj->$_GET['a']($_GET['Code']);
-     $fr = new $_GET['fr']();
-     echo $fr->$_GET['fi']($_GET['Code']);
-         
- ?>  
