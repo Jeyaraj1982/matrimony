d04e71afe959f55be31c183a9ed0bbdce8bce08e
Jeyaraj1982 <38805527+Jeyaraj1982@users.noreply.github.com>
2019-08-22 19:45:06 +0530

    Add files via upload

--- Dashboard/assets/js/mcontroller.js
+++ Dashboard/assets/js/mcontroller.js
@@ -115,29 +115,52 @@
               
     }
     //var obj = jQuery.parseJSON(result);
-    //$('#myModal').modal('show');
+    //$('#myModal').modal('show');                                                                                   
     
-     
-function AddtoFavourite(ProfileCode) {
-                $.ajax({
+     function ResendMobileNumberVerificationForm(frmid1) {
+        
+        var param = $( "#"+frmid1).serialize();
+        $('#Mobile_VerificationBody').html(preloader);
+        $('#myModal').modal('show'); 
+        $.post(API_URL + "m=Member&a=ResendMobileNumberVerificationForm",param,function(result2) {$('#Mobile_VerificationBody').html(result2);});
+    }
+    function ResendEmailVerificationForm(frmid1) {
+        
+        var param = $( "#"+frmid1).serialize();
+        $('#Mobile_VerificationBody').html(preloader);
+        $('#myModal').modal('show'); 
+        $.post(API_URL + "m=Member&a=ResendEmailVerificationForm", param,function(result2) { $('#Mobile_VerificationBody').html(result2); });
+    }
+    
+    function AddtoFavourite(ProfileCode,ImgId) {
+        $('#img_'+ImgId).attr("onclick","javascript:void(0)");
+        var grey = $('#img_'+ImgId).attr("src");
+        var red = $('#img_'+ImgId).attr("src_a");
+        $.ajax({
                 url: API_URL + "m=Member&a=AddToFavourite&ProfileCode="+ProfileCode, 
                 success: function(result){
-                    $('#img_'+ProfileCode).attr("src",$('#img_'+ProfileCode).attr("src_a"));
-                    $('#img_'+ProfileCode).attr("onclick","removeFavourited('"+ProfileCode+"')");
-            }});
-          }
-          
-          function removeFavourited(ProfileCode) {
-                $.ajax({
+                    $('#img_'+ImgId).attr("src",red);
+                    $('#img_'+ImgId).attr("src_a",grey);
+                    $('#img_'+ImgId).attr("onclick","removeFavourited('"+ProfileCode+"','"+ImgId+"')");
+                    $.simplyToast("Profile ID: "+ProfileCode+" has been favourited", 'info');
+        }});
+    }
+    
+    function removeFavourited(ProfileCode,ImgId) {
+        $('#img_'+ImgId).attr("onclick","javascript:void(0)");
+        var grey = $('#img_'+ImgId).attr("src_a");
+        var red = $('#img_'+ImgId).attr("src");
+        $.ajax({
                 url: API_URL + "m=Member&a=AddToFavourite&ProfileCode="+ProfileCode, 
                 success: function(result){
                     
                     if (MyFavoritedPage==1) {
                         $('#div_'+ProfileCode).hide(500);
-                    } else {
-                        $('#img_'+ProfileCode).attr("src",$('#img_'+ProfileCode).attr("src_a")); 
-                        $('#img_'+ProfileCode).attr("onclick","AddtoFavourite('"+ProfileCode+"')");
                     }
-                    
-            }});
-          }
+        
+                    $('#img_'+ImgId).attr("src",grey); 
+                    $('#img_'+ImgId).attr("src_a",red); 
+                    $('#img_'+ImgId).attr("onclick","AddtoFavourite('"+ImgId+"','"+ImgId+"')");
+                    $.simplyToast("Profile ID: "+ProfileCode+" has been unfavourited", 'warning');
+        }});
+    }
