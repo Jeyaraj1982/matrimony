19b9abc992ff6054da4382c9f810df48d84edd4b
Jeyaraj1982 <38805527+Jeyaraj1982@users.noreply.github.com>
2019-08-22 11:41:02 +0530

    Add files via upload

--- Dashboard/views/Franchisee/Members/CreateMember.php
+++ Dashboard/views/Franchisee/Members/CreateMember.php
@@ -122,7 +122,7 @@ function SubmitNewMember() {
                         }
                  
 }                                                
-</script>
+</script>                                                                                                
 <?php 
      $fInfo = $webservice->GetMemberCode(); 
      $MemCode="";
@@ -152,21 +152,40 @@ function SubmitNewMember() {
                             <span class="errorstring" id="ErrMemberName"><?php echo isset($ErrMemberName)? $ErrMemberName : "";?></span>
                           </div>
                         </div>
-                       <div class="form-group row">
-                          <label for="Date of Birth" class="col-sm-2 col-form-label" >Date of Birth<span id="star">*</span></label>
-                          <div class="col-sm-3">
-                            <input type="date" class="form-control" id="DateofBirth" name="DateofBirth" value="<?php echo (isset($_POST['DateofBirth']) ? $_POST['DateofBirth'] : "");?>" placeholder="Date of Birth" style="line-height:15px !important">        
-                            <span class="errorstring" id="ErrDateofBirth"><?php echo isset($ErrDateofBirth)? $ErrDateofBirth : "";?></span>
-                          </div>
-                          <label for="Sex" class="col-sm-2 col-form-label">Sex<span id="star">*</span></label>
+                        <div class="form-group row">
+                            <label for="Name" class="col-sm-2 col-form-label">Date of Birth<span id="star">*</span></label>
+                            <div class="col-sm-1" style="max-width:100px !important;margin-right: -25px;">
+                                    <select class="selectpicker form-control" data-live-search="true" id="date" name="date" style="width:56px">
+                                        <?php for($i=1;$i<=31;$i++) {?>
+                                            <option value="<?php echo $i; ?>" <?php echo ($_POST[ 'date']==$i) ? " selected='selected' " : "";?>><?php echo $i;?>
+                                            </option>
+                                        <?php } ?>
+                                    </select>
+                            </div>
+                            <div class="col-sm-1" style="max-width:100px !important;margin-right: -25px;">        
+                                    <select class="selectpicker form-control" data-live-search="true" id="month" name="month" style="width:56px">
+                                        <?php foreach($_Month as $key=>$value) {?>
+                                            <option value="<?php echo $key+1; ?>" <?php echo ($_POST[ 'month']==$key+1) ? " selected='selected' " : "";?>><?php echo $value;?>
+                                            </option>
+                                        <?php } ?>
+                                    </select>
+                            </div>
+                            <div class="col-sm-2">
+                                    <select class="selectpicker form-control" data-live-search="true" id="year" name="year" style="width:56px">
+                                        <?php for($i=$_DOB_Year_Start;$i>=$_DOB_Year_End;$i--) {?>
+                                            <option value="<?php echo $i; ?>" <?php echo ($_POST['year']==$i) ? " selected='selected' " : "";?>><?php echo $i;?>
+                                            </option>
+                                        <?php } ?>
+                                    </select>
+                            </div>
+                           <label for="Sex" class="col-sm-2 col-form-label" style="text-align: right;">Sex<span id="star">*</span></label>
                             <div class="col-sm-3">
-                          <select class="form-control" id="Sex"  name="Sex">
+                                <select class="form-control" id="Sex"  name="Sex">
                             <?php foreach($fInfo['data']['Gender'] as $Sex) { ?>
                             <option value="<?php echo $Sex['CodeValue'];?>" <?php echo ($_POST['Sex']==$Sex['CodeValue']) ? " selected='selected' " : "";?>> <?php echo $Sex['CodeValue'];?></option>
                             <?php } ?>
                         </select>
-                       <span class="errorstring" id="ErrSex"><?php echo isset($ErrSex)? $ErrSex : "";?></span>
-                     </div> 
+                            </div>
                         </div>
                         <div class="form-group row">
                           <label for="MobileNumber" class="col-sm-2 col-form-label">Mobile Number<span id="star">*</span></label>
